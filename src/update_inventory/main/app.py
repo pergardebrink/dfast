5b74d3b91c06962441bb9f3cf3c490aa368ace92
Per Gårdebrink <per.gardebrink@gmail.com>
2024-11-28 00:53:52 +0100

    Write data to DynamoDB

--- src/update_inventory/main/app.py
+++ src/update_inventory/main/app.py
@@ -1,5 +1,8 @@
+import json
+import boto3
 from confluent_kafka import Consumer, KafkaError
 from appsettings import Settings
+from models.product import Product
 
 settings = Settings()
 
@@ -7,7 +10,10 @@ settings = Settings()
 print("Starting inventory consumer...")
 print(f"Kafka: {settings.kafka_bootstrap_servers}")
 print(f"Inventory Topic: {settings.inventory_topic}")
+print(f"DynamoDB: {settings.dynamodb_url}")
 
+dynamodb = boto3.resource('dynamodb', endpoint_url=settings.dynamodb_url)
+table = dynamodb.Table(settings.dynamodb_table)
 
 consumer_config = {
     'bootstrap.servers': settings.kafka_bootstrap_servers,
@@ -17,6 +23,14 @@ consumer_config = {
 
 consumer = Consumer(consumer_config)
 consumer.subscribe([settings.inventory_topic])
+
+
+def process_message(message):
+    product_data = json.loads(message.value().decode('utf-8'))
+    product = Product(**product_data)
+    table.put_item(Item=product.model_dump())
+    # TODO: Handle out of order updates to only replace if newer?
+
 
 try:
     while True:
@@ -30,6 +44,7 @@ try:
                 print(msg.error())
                 break
         print(f"Message received: {msg.value().decode('utf-8')}")
+        process_message(msg)
 finally:
     consumer.close()
     print("Consumer closed")
--- src/update_inventory/main/appsettings.py
+++ src/update_inventory/main/appsettings.py
@@ -3,4 +3,6 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     kafka_bootstrap_servers: str
+    dynamodb_url: str
+    dynamodb_table: str
     inventory_topic: str
